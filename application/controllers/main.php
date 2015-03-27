e08e224d5fdda0b2f8e224501c2203334d324be7
Anton Golomazov <agolomazov@itloft.ru>
2015-03-27 20:14:50 +0300

    Add send message to admin

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -11,16 +11,24 @@ class Main extends MY_Controller {
             $file_upload = $this->downloadfile($file);
 
             if($file_upload){
+
+                // Отправка письма пользователю
                 $email = $this->input->post('email');
                 $subject = "Спасибо вам за ваш заказ";
                 $text = "Спасибо вам за ваш заказ";
-
-                $this->sendmailer->addAttachment($file_upload);
-
                 $this->sendmailer->setSenderLabel('PSDTOHTML4YOU');
                 $this->sendmailer->setRecipient($email, "Заказчик");
                 $this->sendmailer->send($subject, $text);
+
+                // Письмо администратору
+                $subject = "Новый заказ на верстку проекта";
+                $text = "Пришел заказ на верстку с почты <a href='mailto:{$email}'>{$email}</a>";
+                $this->sendmailer->setSenderLabel('PSDTOHTML4YOU');
+                $this->sendmailer->setRecipient($this->config->item('email_user'), "Администратор");
+                $this->sendmailer->addAttachment($file_upload);
+                $this->sendmailer->send($subject, $text);
                 unlink($file_upload);
+
             }
 
             redirect('/');
